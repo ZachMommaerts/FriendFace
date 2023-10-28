6dae29eca3e116b48b3f4fd500c96053d7e4002d
ZachMommaerts <zmommaerts@gmail.com>
2023-10-28 15:45:43 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Zach Mommaerts on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
